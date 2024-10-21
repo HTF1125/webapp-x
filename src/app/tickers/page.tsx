4b20b5e109697dae97441529ca05868d6b64afda
hantianfeng@outlook.com <hantianfeng@outlook.com>
2024-10-21 23:00:49 +0900

    2024-10-21-2

--- src/app/tickers/page.tsx
+++
@@ -1,7 +0,0 @@
-import TickersClient from './TickersClient';
-import { fetchTickers } from "./api";
-
-export default async function TickersPage() {
-  const initialTickers = await fetchTickers();
-  return <TickersClient initialTickers={initialTickers} />;
-}
