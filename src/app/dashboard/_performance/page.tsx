b87e0360998bc2df53d54a9cf6a527c06eb2ee8e
hantianfeng@outlook.com <hantianfeng@outlook.com>
2025-01-01 19:17:24 +0900

    20241231-3

--- src/app/dashboard/_performance/page.tsx
+++ src/app/dashboard/_performance/page.tsx
@@ -3,35 +3,47 @@
 import React from "react";
 import { PerformanceGrouped } from "./types";
 import ClientPerformancePage from "./ClientPerformance";
+import { NEXT_PUBLIC_API_URL } from "@/config";
 
+// Function to fetch performance data
 async function fetchPerformanceGrouped(): Promise<PerformanceGrouped[]> {
+  const endpoint = new URL("/api/performances-grouped", NEXT_PUBLIC_API_URL);
+
   try {
-    const response = await fetch(
-      "http://localhost:3000/api/performance-grouped",
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
+    const response = await fetch(endpoint.toString(), {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
 
     if (!response.ok) {
-      throw new Error(`Failed to fetch data: ${response.statusText}`);
+      const errorData = await response.json();
+      throw new Error(errorData?.message || "Failed to fetch index groups.");
     }
 
-    const data: PerformanceGrouped[] = await response.json();
-    return data;
+    const data = await response.json();
+    return data; // Return the data directly
   } catch (error) {
-    console.error("Error fetching index group performances:", error);
+    console.error("Error fetching performance data:", error);
     throw error;
   }
 }
 
 const PerformancePage = async () => {
-  const performanceGrouped = await fetchPerformanceGrouped();
-
-  return <ClientPerformancePage performanceGrouped={performanceGrouped} />;
+  try {
+    const performanceGrouped = await fetchPerformanceGrouped();
+    return <ClientPerformancePage performanceGrouped={performanceGrouped} />;
+  } catch (error) {
+    // Render error state or fallback UI
+    console.error("Error rendering PerformancePage:", error);
+    return (
+      <div className="error-message">
+        <p>Failed to load performance data. Please try again later.</p>
+      </div>
+    );
+  }
 };
 
 export default PerformancePage;
